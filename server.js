e8d1a5ba6c4781e52acf108de17acc3fafdf7a46
jbohrer314 <53622645+jbohrer314@users.noreply.github.com>
2022-12-04 20:38:45 -0800

    Added rendering for main site and error page

---
+++ server.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars')
+// var postData = require("./postData.json")
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: "main"
+}))
+
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res) {
+  res.status(200).render("metronome")
+})
+
+// app.get("/posts/:post_number", function(req, res, next) {
+//   var post_number = req.params.post_number
+//   var post_content = postData[post_number]
+//   if (post_content) {
+//     res.status(200).render("singlePost", post_content)
+//     console.log("rendering post number", post_number, "from handlebars")
+//   }
+//   else {
+//     next()
+//   }
+// })
+
+app.get('*', function (req, res) {
+  res.status(404).render("404")
+  console.log("rendering 404 page")
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
